27d3f773e780bbdf8e80bdb68a6e8cf104de5cf1
Groove <63636914+Groovemaker@users.noreply.github.com>
2021-12-04 06:44:48 +0100

    Update heimdall.lua

--- lua/deus/core/heimdall.lua
+++ lua/deus/core/heimdall.lua
@@ -1,5 +1,6 @@
 -- DEUS MODULE: "HEIMDALL" THREAT ASSESSMENT
 -- (c) 2021 Zeitgeist Studios
+-- Purpose: Footprinting, profiling people and comparing them against a threat level.
 -- Your Hosts: ["Runic"]
 
 -- Init Main System and its Tables.
